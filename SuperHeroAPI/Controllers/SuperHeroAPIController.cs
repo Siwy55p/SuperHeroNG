977080205751bcb095bbe403d9d77d3d53c9f822
Siwy55p <szuminski.p@gmail.com>
2023-01-04 21:54:44 +0100

    firstcommit

---
+++ SuperHeroAPI/Controllers/SuperHeroAPIController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroAPIController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetSuperHeroes()
+        {
+            return new List<SuperHero>()
+            {
+                new SuperHero
+                {
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "NEw Yourk City"
+                }
+            };
+
+        }
+    }
+}
